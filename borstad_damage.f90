dfbbb5b57c48fcf134bf7277b3ce1bc51710b2bc
David Lilien <dlilien90@gmail.com>
2016-05-09 10:31:27 -0700

    No values checked, but borstad_damage running w/o Error

--- borstad_damage.f90
+++ borstad_damage.f90
@@ -63,7 +63,43 @@ FUNCTION EnhancementFactor ( Model, nodenumber, D) RESULT(E)
   ! write(*,*)'E', E
 END FUNCTION EnhancementFactor
 
+FUNCTION EnhancedEta ( Model, nodenumber, D) RESULT(Visc)
+   USE types
+   USE CoordinateSystems
+   USE SolverUtils
+   USE ElementDescription
+   USE DefUtils
+   IMPLICIT NONE
+   TYPE(Model_t) :: Model
+   TYPE(ValueList_t), POINTER :: Material
+   TYPE(Solver_t), TARGET :: Solver
+   REAL(KIND=dp) :: D(2), Visc, n  
+   INTEGER :: nodenumber
+   LOGICAL :: FirstTime=.TRUE., GotIt
 
+   SAVE FirstTime, n 
+
+   IF (FirstTime) THEN
+   FirstTime = .False.
+    
+      Material => GetMaterial()
+      n = GetConstReal( Material, 'Glen Exponent', GotIt )
+      IF (.NOT.GotIt) THEN
+         WRITE(Message,'(A)') 'Variable Glen Exponent not found. &
+              &Setting to 3.0'
+         CALL INFO('Damage EnhancementFactor', Message, level=2)
+         n = 3.0_dp
+      ELSE
+         WRITE(Message,'(A,F10.4)') 'n = ', n 
+         CALL INFO('Damage EnhancementFactor', Message, level=2)
+      END IF
+   END IF
+
+       Visc = D(1)**2.0_dp * (1.0_dp - D(2))**(-n) 
+    
+  ! write(*,*) D
+  ! write(*,*)'E', E
+END FUNCTION EnhancedEta
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !
 ! SourceDamage 
@@ -71,8 +107,6 @@ END FUNCTION EnhancementFactor
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 FUNCTION SourceDamage (Model, nodenumber, D) RESULT(Source)
-
-
    USE types
    USE CoordinateSystems
    USE SolverUtils
@@ -81,7 +115,7 @@ FUNCTION SourceDamage (Model, nodenumber, D) RESULT(Source)
    USE GeneralUtils
    IMPLICIT NONE
    TYPE(Model_t) :: Model
-   REAL (KIND=dp) :: D, Source          
+   REAL (KIND=dp) :: D, Source         
    INTEGER :: nodenumber
  
    TYPE(Solver_t):: Solver 
@@ -96,7 +130,7 @@ FUNCTION SourceDamage (Model, nodenumber, D) RESULT(Source)
    INTEGER :: Ind(3,3), DIM, i, j, indice(3), infor
    REAL (KIND=dp) :: Sig(3,3), SigDev(3,3), tmp 
    REAL (KIND=dp) :: Eps(3,3)
-   REAL (KIND=dp) :: Chi, B, pwater
+   REAL (KIND=dp) :: Chi 
    Real (KIND=dp) :: EffectiveStress, EffectiveStrainRate, Kappa
    REAL (KIND=DP) :: EI(3),Dumy(1),Work(24)
    REAL (KIND=DP) :: EpsilonZero, TauZero
@@ -104,7 +138,7 @@ FUNCTION SourceDamage (Model, nodenumber, D) RESULT(Source)
    CHARACTER*20 :: USF_Name='SourceDamage'
 
 
-   SAVE :: Ind, DIM, B
+   SAVE :: Ind, DIM
    SAVE :: FirstTime, Cauchy
    SAVE :: TauZero, EpsilonZero, Kappa
    
@@ -126,14 +160,6 @@ FUNCTION SourceDamage (Model, nodenumber, D) RESULT(Source)
     Material => GetMaterial()
     !Read the coefficients B and sigmath  
 
-    B = GetConstReal( Material, 'Damage Enhancement Factor', GotIt )
-      IF (.NOT.GotIt) THEN
-      CALL FATAL('Damage Source', 'Damage Enhancement Factor B not Found')
-      ELSE
-         WRITE(Message,'(A,F10.4)') 'Damage Enhancement Factor = ', B
-         CALL INFO('Damage Source', Message, level=2)
-      ENDIF
-
    ! Cauchy or deviatoric stresses ?
       Cauchy = ListGetLogical( Material , 'Cauchy', Gotit )
       WRITE(Message,'(A,L1)') 'Cauchy stress tensor computed ? ', Cauchy 
@@ -151,12 +177,13 @@ FUNCTION SourceDamage (Model, nodenumber, D) RESULT(Source)
         IF (.NOT.GotIt) THEN
           Kappa = 2.8_dp
           CALL INFO('USF_Damage','No "Borstad Kappa" given, set &
-          to 0.130', level=2)
+          to 2.8_dp', level=2)
         END IF
    ! For now we just use pick a temperature value to calculate varepsilon_0
    ! Go with T=-10 from C&P, :: 3.5e-25 s^-1 Pa^-3
    ! So in a^-1 MPa^-3 this is
    EpsilonZero = 3.5e-25_dp * 1.0e18_dp * 365.25_dp * 24.0_dp * 60.0_dp * 60.0_dp * TauZero**3
+   Call Info('Damage Soure', 'First time completed', level=7)
    END IF ! FirstTime
 
 
@@ -175,20 +202,12 @@ FUNCTION SourceDamage (Model, nodenumber, D) RESULT(Source)
    ChiPerm    => ChiVariable % Perm
    ChiValues  => ChiVariable % Values
    
-   ! Get the Sea Damage Pressure
-   PSeaDVariable => VariableGet( Model % Variables, 'PSeaD' )
-   IF ( ASSOCIATED( PSeaDVariable ) ) THEN
-      PSeaDPerm   => PSeaDVariable % Perm
-      PSeaDValues => PSeaDVariable % Values
-   ELSE
-      CALL WARN('Damage Source', 'PSeaD not associated, basal pressure not taken into account in damage formation' )
-      CALL WARN('Damage Source', 'Taking default value PSeaD=0.0')
-   END IF
-
    ! Get the variables to compute the hydrostatic pressure  
    FlowVariable => VariableGet( Model % Variables, 'Flow Solution')
    FlowPerm    => FlowVariable % Perm
    FlowValues  => FlowVariable % Values
+
+   CALL Info('Damage Source', 'Stress, Strain, Chi, and Flow Found', level=7)
 
    Sig = 0.0
    DO i=1, DIM
@@ -229,6 +248,9 @@ FUNCTION SourceDamage (Model, nodenumber, D) RESULT(Source)
    EffectiveStrainRate = SQRT(0.5_dp * (Eps(1,1)**2.0_dp + Eps(2,2)**2.0_dp + Eps(3,3)**2.0_dp + &
        Eps(3,1)**2.0_dp + Eps(2,1)**2.0_dp + Eps(3,2)**2.0_dp))
    
+
+   CALL Info('Damage Source', 'Effective Stress and Strain Computed', level=8)
+   
    IF (EffectiveStress<TauZero) THEN
        Chi = 0.0_dp
    ELSE
@@ -238,7 +260,12 @@ FUNCTION SourceDamage (Model, nodenumber, D) RESULT(Source)
    END IF
 
    ChiValues(ChiPerm(nodenumber)) = Chi
- 
-   Source = Chi
 
+   Call Info('Damage Source', 'Chi Computed', level=9)
+
+   IF (D > chi) THEN
+       source = 0.0_dp
+   ELSE
+       source = D - chi
+   END IF
 END FUNCTION SourceDamage   
