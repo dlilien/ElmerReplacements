5c18db14f3a7e92df02108c3b8e23c70884d6a2d
dlilien <skinonframe@gmail.com>
2017-06-26 17:11:41 -0700

    read_routines.f90: read list of points in binary

--- read_routines.f90
+++ read_routines.f90
@@ -7,6 +7,37 @@
 
       module read_routines
           contains
+          subroutine get_dat(fn, x, y, z, dat)
+              implicit None
+              character (len=128):: fn
+              integer i, r, np
+              real, dimension(:), allocatable :: x, y, z, dat
+              open(10, access='DIRECT', recl=4, file=fn)
+              read(10, rec=1) np
+              allocate (x(np), y(np), z(np), dat(np))
+              r=2
+              do i=1,np
+                  read(10, rec=r) x(i)
+                  r = r+1
+              end do
+              write(*,*) x(1), x(np)
+              do i=1,np
+                  read(10, rec=r) y(i)
+                  r = r+1
+              end do
+              write(*,*) y(1), y(np)
+              do i=1,np
+                  read(10, rec=r) z(i)
+                  r = r+1
+              end do
+              write(*,*) z(1), z(np)
+              do i=1,np
+                  read(10, rec=r) dat(i)
+                  r = r+1
+              end do
+              write(*,*) dat(1), dat(np)
+          end
+
           subroutine get_threed_grid(fn, x, y, z)
               implicit None
               character (len=128):: fn
