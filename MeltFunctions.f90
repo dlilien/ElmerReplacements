ab5070aa68e0727f51233b8c09fd6cffea0f4a05
David Lilien <dlilien90@gmail.com>
2017-08-17 14:29:50 -0700

    add sheans melt function

--- MeltFunctions.f90
+++ MeltFunctions.f90
@@ -47,6 +47,25 @@
         return
         END
 
+        FUNCTION BasalMeltShean( model, nodenumber, z) RESULT(melt)
+        Use Types
+        implicit none
+        Real(kind=dp) :: melt
+        Real(kind=dp) :: z(2)
+        Type(Model_t) :: model
+        Integer :: nodenumber
+        IF (Z(2) > 0.5_dp) THEN
+            melt = 0_dp
+        ELSE
+            IF (z(1)<=-400_dp) THEN
+                melt = -0.182_dp * (z(1) + 400.0_dp)
+            ELSE
+                melt = 0.0_dp
+            END IF
+        END IF
+        return
+        END
+
         FUNCTION BasalMeltZero( model, nodenumber, z) RESULT(melt)
         Use Types
         implicit none
@@ -111,5 +130,24 @@
         Type(Model_t) :: model
         Integer :: nodenumber
         melt = 0_dp
+        return
+        END
+
+        FUNCTION BasalMeltShean_negative( model, nodenumber, z) RESULT(melt)
+        Use Types
+        implicit none
+        Real(kind=dp) :: melt
+        Real(kind=dp) :: z(2)
+        Type(Model_t) :: model
+        Integer :: nodenumber
+        IF (Z(2) > 0.5_dp) THEN
+            melt = 0_dp
+        ELSE
+            IF (z(1)<=-400_dp) THEN
+                melt = 0.182_dp * (z(1) + 400.0_dp)
+            ELSE
+                melt = 0.0_dp
+            END IF
+        END IF
         return
         END
